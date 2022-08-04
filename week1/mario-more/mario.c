c5ef0922c0b68fa6fad28c8cd810cf5bee0b1348
LouisaStrunk02 <louisa.strunk@5minds.de>
2022-08-04 12:51:28 +0200

    add mario-more

---
+++ week1/mario-more/mario.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("height: ");
+    } while (h < 1 || h > 8);
+
+    // for each row
+    for (int i = 0; i < h; i++)
+    {
+        // for each column
+        for (int j = 0; j <= h-i; j++)
+        {
+            //print nothing
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            // print a brick
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j <= i; j++)
+        {
+            // print a brick
+            printf("#");
+        }
+
+        // move to next row
+        printf("\n");
+    }
+}
